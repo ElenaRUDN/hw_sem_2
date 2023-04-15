f6e779f26e9653481b0f33cc8fcb060f6d05208b
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-04-15 16:07:48 +0300

    Выполненное 1 задание

--- laba6/hw6ex1.c
+++ laba6/hw6ex1.c
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 
 int main(){
+    char sim;
+    scanf("%c", &sim);
     int i = 0;
     int k = 1;
     int maximk = 0;
     char arr[9] = {'U','R','R','R','B','T','T','T','T'};
     for(; i<8; i++){
-        if (arr[i] == arr[i+1]){
+        if (arr[i] == sim & arr[i] == arr[i+1]){
             k++;
             if (k > maximk){
                 maximk = k;
