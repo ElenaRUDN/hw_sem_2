2a0707113f925ff08ea8f6c75b7c9a71fb7236ef
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-04-16 22:52:24 +0300

    процесс 3 задания №3

--- laba6/hw6ex3.c
+++ laba6/hw6ex3.c
@@ -5,25 +5,32 @@
 #include <stdlib.h>
 #include <time.h>
 
+void append(int arr[], int max_razm, int elements);
+void print(int arr[], int max_razm, int elements);
+void append(int arr[], int max_razm, int elements);
+
 int main(){
     int max_razm;
-    int element;
+    int elements;
     int com;
     int i = 0;
+    int j = 0;
+    int el;
+    int mesto;
 
     printf("%s\t", "Введите максимальный размер массива:");
     scanf("%d", &max_razm);
     printf("%s\t", "Введите количество элементов, которые вы хотите заполнить:");
-    scanf("%d", &element);
+    scanf("%d", &elements);
 
-    if(max_razm < element){
+    if(max_razm < elements){
         printf("%s\n","Вы превысили допустимый размер массива.");
         exit(EXIT_SUCCESS);
     }
     else{
         int arr[max_razm];
 
-        for(; i < element; i++){
+        for(; i < elements; i++){
             arr[i] = rand()%100;
         }
         
@@ -32,11 +39,31 @@ int main(){
         printf("%s\n","2. Добавить элемент в конец массива.");
         printf("%s\n","3. Добавить элемент в середину массива.");
         printf("%s\n","Введите номер команды:");
-        scanf("%d\n", &com);
+
+        scanf("%d", &com);
         if(com == 1){
-            for(; i < max_razm; i++){
-                printf("%d\t", arr[i]);
+            for(; j < max_razm; j++){
+                printf("%d\t", arr[j]);
             }
+        }
+        else if(com == 2){
+            printf("%s\n","Введите элемент:");
+            scanf("%d", &el);
+            arr[max_razm] = el;
+            for(; j < max_razm; j++){
+                printf("%d\t", arr[j]);
+            }
+        }
+        else if(com == 3){
+            printf("%s\n","Введите элемент:");
+            scanf("%d", &el);
+            printf("%s\n","Введите место:");
+            scanf("%d", &mesto);
+            arr[mesto] == el;
+            for(; j < max_razm; j++){
+                printf("%d\t", arr[j]);
+            }
+            
         }
     }
     return 0;
