976c41861d2dec08fd5801dcfacdc313a6418b26
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-04-16 23:25:05 +0300

    Процесс 3 задания №4

--- laba6/hw6ex3.c
+++ laba6/hw6ex3.c
@@ -6,16 +6,15 @@
 #include <time.h>
 
 void append(int arr[], int max_razm, int elements);
-void print(int arr[], int max_razm, int elements);
-void append(int arr[], int max_razm, int elements);
+void print(int arr[], int max_razm);
+void insert(int arr[], int max_razm, int elements);
 
 int main(){
     int max_razm;
     int elements;
     int com;
     int i = 0;
-    int j = 0;
-    int el;
+    int dop_elem;
     int mesto;
 
     printf("%s\t", "Введите максимальный размер массива:");
@@ -30,42 +29,34 @@ int main(){
     else{
         int arr[max_razm];
 
-        for(; i < elements; i++){
+        for(i = 0; i < elements; i++){
             arr[i] = rand()%100;
         }
-        
-        printf("%s\n","Ваши команды:");
-        printf("%s\n","1. Отобразить массив на экране.");
-        printf("%s\n","2. Добавить элемент в конец массива.");
-        printf("%s\n","3. Добавить элемент в середину массива.");
-        printf("%s\n","Введите номер команды:");
-
-        scanf("%d", &com);
-        if(com == 1){
-            for(; j < max_razm; j++){
-                printf("%d\t", arr[j]);
-            }
+        for(i = elements; elements < max_razm; i++){
+            arr[i] = 0;
         }
-        else if(com == 2){
-            printf("%s\n","Введите элемент:");
-            scanf("%d", &el);
-            arr[max_razm] = el;
-            for(; j < max_razm; j++){
-                printf("%d\t", arr[j]);
-            }
+        printf("%s\n","Добавление в конец массива:");
+        for(i = 0; i < 5; i++){
+            append(arr, max_razm, i);
         }
-        else if(com == 3){
-            printf("%s\n","Введите элемент:");
-            scanf("%d", &el);
-            printf("%s\n","Введите место:");
-            scanf("%d", &mesto);
-            arr[mesto] == el;
-            for(; j < max_razm; j++){
-                printf("%d\t", arr[j]);
-            }
-            
-        }
+        printf("%s\n","Добавление в середину массива:");
+        for(i = 0; i < 5; i++){
+            insert(arr, max_razm, i);
+        }    
     }
     return 0;
+}
 
+void print(int arr[], int max_razm){
+    for(int j = 0; j < max_razm; j++){
+        printf("%d\t", arr[j]);
+    }
+}
+void append(int arr[], int max_razm, int dop_elem){
+    arr[max_razm] = dop_elem;
+    print(arr, max_razm);
+}
+void insert(int arr[], int max_razm, int dop_elem){
+    arr[max_razm/2] == dop_elem;
+    print(arr, max_razm);
 }
