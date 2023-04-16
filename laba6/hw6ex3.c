f0d6b3fa6e6e0d83d71e60293c83a9897716052e
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-04-16 22:12:23 +0300

    Процесс 3 задания №2

--- laba6/hw6ex3.c
+++ laba6/hw6ex3.c
@@ -8,16 +8,36 @@
 int main(){
     int max_razm;
     int element;
-    //printf("%s\t", "Введите максимальный размер массива:");
+    int com;
+    int i = 0;
+
+    printf("%s\t", "Введите максимальный размер массива:");
     scanf("%d", &max_razm);
-    //printf("%s\t", "Введите количество элементов, которые вы хотите заполнить:");
+    printf("%s\t", "Введите количество элементов, которые вы хотите заполнить:");
     scanf("%d", &element);
+
     if(max_razm < element){
         printf("%s\n","Вы превысили допустимый размер массива.");
         exit(EXIT_SUCCESS);
     }
     else{
-        printf("%s\n","Вы молодцы");
+        int arr[max_razm];
+
+        for(; i < element; i++){
+            arr[i] = rand()%100;
+        }
+        
+        printf("%s\n","Ваши команды:");
+        printf("%s\n","1. Отобразить массив на экране.");
+        printf("%s\n","2. Добавить элемент в конец массива.");
+        printf("%s\n","3. Добавить элемент в середину массива.");
+        printf("%s\n","Введите номер команды:");
+        scanf("%d\n", &com);
+        if(com == 1){
+            for(; i < max_razm; i++){
+                printf("%d\t", arr[i]);
+            }
+        }
     }
     return 0;
 
