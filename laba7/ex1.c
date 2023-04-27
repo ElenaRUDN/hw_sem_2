0ba9fc22d3b1dc61a4ef3cf5074212683e5a11dc
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-04-27 17:50:29 +0300

    процесс 2 задания №1

--- laba7/ex1.c
+++ laba7/ex1.c
@@ -3,19 +3,28 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+void print(char* words_array);
 
-int main(){
+void print(char* words_array) {
+    int i;
+    for (i = 0; i < strlen(words_array); i++) {
+        printf("%c", words_array[i]);
+    }
+    printf("\n");
+}
+
+void main(){
     int i;
     int j;
-    char* words_array[10];
+    char* words_array;
     char word[50];
-    for(i =0; i < 10; i++){
+    for(i = 0; i < 10; i++){
+        printf("Введите слово: ");
         scanf("%s", &word);
         words_array[i] = word;
-        printf("%s\n", word);
+        print(words_array[i]);
     }
-    
-    printf("%s\n", words_array);
+    strcpy(words_array, word);
     return 0;
 }
 
@@ -24,51 +33,3 @@ int main(){
 
 
 
-
-
-
-//     char* words[ARRAY_SIZE];
-//     int i;
-//     int j;
-//     for (int i = 0; i < ARRAY_SIZE; i++) {
-//         words[i] = NULL;
-//     }
-
-//     char tmp[MAX_LEN];//временный массив для слова
-    
-//     for(int i = 0; i < ARRAY_SIZE ; i++){
-//         scanf("%s", &tmp);
-        
-//         // ключевое слово сделайте "X" для выхода
-//         if (strlen(tmp) == 1 && tmp[0] == 'X')  {
-//             break;  
-//         }
-          
-//         //узнаю размер слова в массиве tmp и увеличиваю words[i]
-//         // память выделяли мало, не хватает для конечного символа конца строки '\00'
-//         words[i] = (char *)malloc(sizeof(char)*(strlen(tmp) + 1));
-//         if(words[i] == NULL){
-//             printf("fail");
-//             exit(0);
-//         }
-
-//         // указатель на выделенную память вы меняете на адрес локального массива
-//         // words[i] = tmp;
-//         // копируем строку из tmp в words[i]
-//         strcpy(words[i],tmp);
-//     }
-
-//     // free(words);
-//     // удалять локальный массив вы не имеете права, вы память для него не выделяли
-//     // всю память, что вы выделяли, от той и отказывайтесь
-//     for(int i = 0; i < words_N ; i++){
-//         free (words[i]);
-//     }
-//     for(int i = 0; i < ARRAY_SIZE; i++) {
-//         for(int j = 0; j < MAX_LEN; j++) {
-//             printf("%c", words[i][j]);
-//         }
-//         printf("\n");
-//     } 
-//     return 0;
-// }
