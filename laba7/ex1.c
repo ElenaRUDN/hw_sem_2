0fb5142d76ed70b9988cf5c7d9724347664ae1cf
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-04-27 18:26:54 +0300

    готовое задание №1

--- laba7/ex1.c
+++ laba7/ex1.c
@@ -3,33 +3,98 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-void print(char* words_array);
+void print(char* arr);
+void swap_chars(char* x, char* y);
+void selectionSort(char* arr);
+void bubbleSort(char* arr);
+void combSort(char* arr);
+void getWords(char* arr);
 
-void print(char* words_array) {
+int main(){
+    int i = 10;
+    int j = 10;
+    int n = 10;
+    char word[30];
+    char** words_array = (char**)malloc(sizeof(char*)*n);
+    for(i = 0; i < n; i++) {
+        words_array[i] = (char *)malloc(sizeof(char));
+        getWords(words_array[i]);
+        bubbleSort(words_array[i]);
+        print(words_array[i]);
+        selectionSort(words_array[i]);
+        print(words_array[i]);
+        combSort(words_array[i]);
+        print(words_array[i]);
+    }
+    return 0;
+}
+
+void getWords(char* arr) {
+    char word[30];
+    printf("Введите слово: ");
+    scanf("%s", &word);
+    strcpy(arr, word);
+}
+
+void print(char* arr) {
     int i;
-    for (i = 0; i < strlen(words_array); i++) {
-        printf("%c", words_array[i]);
+    for (i = 0; i < strlen(arr); i++) {
+        printf("%c", arr[i]);
     }
     printf("\n");
 }
 
-void main(){
-    int i;
-    int j;
-    char* words_array;
-    char word[50];
-    for(i = 0; i < 10; i++){
-        printf("Введите слово: ");
-        scanf("%s", &word);
-        words_array[i] = word;
-        print(words_array[i]);
-    }
-    strcpy(words_array, word);
-    return 0;
+void swap_chars(char* x, char* y) {
+	char helping;
+	helping = *x;
+	*x = *y;
+	*y = helping;
 }
 
+void bubbleSort(char* arr) {
+    int i, j, lena = strlen(arr);
+    for (i = 0; i < lena - 1; i++) {
+        for (j = 0; j < lena - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap_chars(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+    printf("%s\t","Сортировка пузырьком: ");
 
+}
 
+void selectionSort(char* arr) {   
+    int i;
+    int j;
+    int min_index;
+    int lena = strlen(arr);
+    for (i = 0; i < lena-1; i++) {
+        min_index = i;
+        for (j = i+1; j < lena; j++)
+        {
+          if (arr[j] < arr[min_index])
+              min_index = j;
+        }
+        if (min_index!=i)
+            swap_chars(&arr[min_index], &arr[i]);
+    }
+    printf("%s\t","Сортировка выбором: ");
+}
 
+void combSort(char* arr) {
+    int i, j, length = strlen(arr);
+    int distance = length / 1.247;
+    while(distance > 0) {
+        
+        for (j = 0; j < length - distance; j++) {
+            if (arr[j] > arr[j + distance]) {
+                swap_chars(&arr[j], &arr[j + distance]);
+          }
+        }
+        distance = distance / 1.247;
+    }
+    printf("%s\t","Сортировка расческой: ");
+}
 
 
