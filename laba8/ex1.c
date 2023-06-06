ef99c7418dd969b7f620cedacfe61d744e46d399
ElenaRUDN <126961213+ElenaRUDN@users.noreply.github.com>
2023-06-06 12:16:52 +0300

    ex1 with addition

--- laba8/ex1.c
+++ laba8/ex1.c
@@ -4,35 +4,41 @@
 #include <string.h>
 #include <math.h>
 
-//void funcRp(float rp);
-
 int main(){
     float rp, we, ra1, nu, i;
     float va, vw;
+    int rahelp;
+    int count = 9;
     we = 7.2921159*pow(10,-5);
-    i = 63.4;
+    i=(63.4*3.14)/180;
     ra1 = 45000;
     nu = 398600.4;
     rp = (pow(we,2)* pow(ra1,4) * pow(cos(i),2))/(2*nu - pow(we,2)*pow(ra1,3)*pow(cos(i),2));
-    printf("%f\n", rp);
+    //printf("%f\n", rp);
     FILE* file = fopen("laba8.txt", "w+");
     for (int ra = 1; ra <= 60000; ra+=10){
-            va = sqrt((2*nu*rp)/(ra*(ra - rp)));
-            vw = we * ra*cos(i);
-            if (va != vw){
-                fprintf(file, "%.3f", roundf(va * 100) / 100);
-                fprintf(file, "%.3f", roundf(vw * 100) / 100);
+        va = sqrt((2*nu*rp)/(ra*(ra + rp)));
+        vw = we * ra*cos(i);
+        if (va > vw){
+            fprintf(file, "%f\n", va);
+            fprintf(file, "%f\n", vw);
+        }
+        else if (va <= vw){
+            //printf("%d\n", ra);
+            while (count > 0){
+                rahelp = ra - count;
+                count = count - 1;
+                va = sqrt((2*nu*rp)/(rahelp*(rahelp + rp)));
+                vw = we * rahelp*cos(i);
+                if (va > vw){
+                    fprintf(file, "%f\n", va);
+                    fprintf(file, "%f\n", vw);
+                }
             }
+            break;
+        }
     }
+    fclose(file);
     return 0;
 }
 
-// void funcRp(float rp){
-//     float rp, we, ra, nu, i;
-//     we = 7.2921159*0,00001;
-//     i = 63.4;
-//     ra = 45000;
-//     nu = 398600.4;
-//     rp = (we*we*ra*ra*ra*ra* cos(i)* cos(i)) / (2*nu - we*we*ra*ra*ra*cos(i)*cos(i));
-//     printf("%d", rp);
-// }
